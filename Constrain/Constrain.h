46e522d17198fe95a594cbb62ad0f53947e91617
dorukcoban <dorukcoban@teknasyon.com>
2021-08-23 10:44:38 +0300

    Initial Commit

---
+++ Constrain/Constrain.h
@@ -0,0 +1,18 @@
+//
+//  Constrain.h
+//  Constrain
+//
+//  Created by Doruk Çoban on 23.08.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Constrain.
+FOUNDATION_EXPORT double ConstrainVersionNumber;
+
+//! Project version string for Constrain.
+FOUNDATION_EXPORT const unsigned char ConstrainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Constrain/PublicHeader.h>
+
+
